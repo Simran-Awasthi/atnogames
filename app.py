b38e9957dfe067cce52e5f60da4767346ad4061c
Simran-Awasthi <simranawasthi03@gmail.com>
2021-05-24 03:14:56 +0530

    Home Page Built

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 import json, os
-
+from get_data import get_game_details
 app = Flask(__name__, template_folder='./templates/', static_folder='./static/')
 
 @app.route('/')
@@ -11,8 +11,13 @@ def hello_world():
 def get_games():
     with open('game_info.json', 'r') as f:
         data = json.load(f)
-    return render_template('games_view.html', games=data['games_list'])
+    return render_template('games_view.html', games = data['games_list'])
 
+@app.route('/details')
+def get_data():
+    link = request.args.get('url')
+    data = get_game_details(link)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run()
